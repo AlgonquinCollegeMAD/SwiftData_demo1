a6ec10207fa41db60509ad60b439d6061f242e22
Vladimir Cezar <vladimircezar@icloud.com>
2024-11-20 14:48:58 -0500

    Remove id property from Task model

--- SwiftData_demo/Task.swift
+++ SwiftData_demo/Task.swift
@@ -11,12 +11,11 @@ import SwiftData
 
 @Model
 class Task {
-    var id: UUID
     var title: String
     var isCompleted: Bool
+    @Relationship(inverse: \Project.tasks) var project: Project?
 
     init(title: String, isCompleted: Bool = false) {
-        self.id = UUID()
         self.title = title
         self.isCompleted = isCompleted
     }
