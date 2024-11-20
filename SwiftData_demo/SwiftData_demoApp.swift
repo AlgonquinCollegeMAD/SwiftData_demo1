5e216689bcdfff27338ade2f63c885274537e845
Vladimir Cezar <vladimircezar@icloud.com>
2024-11-20 14:15:06 -0500

    Initial Commit

---
+++ SwiftData_demo/SwiftData_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData_demoApp.swift
+//  SwiftData_demo
+//
+//  Created by Vladimir Cezar on 2024.11.20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
